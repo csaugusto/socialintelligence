1b270bfe504a4dd6877efbd53c3334021ed69174
Carlos Sanchez <carlos@thedive.studio>
2026-04-12 00:27:09 -0600

    quitando pipeline

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,16 +13,5 @@ module.exports = {
         PORT:     3001,
       },
     },
-    {
-      name:   'si-pipeline',
-      script: './src/index.js',
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      cron_restart: '*/15 * * * *', // fallback si el cron interno falla
-      env: {
-        NODE_ENV: 'production',
-      },
-    },
   ],
 };
